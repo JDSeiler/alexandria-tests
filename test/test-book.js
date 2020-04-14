db404db7228ae5b755f6ec35ba247cc65cee58e7
JDSeiler <jorsei1p217@gmail.com>
2020-04-13 20:12:45 -0400

    Finish writing tests for all GET endpoints

--- test/test-book.js
+++ test/test-book.js
@@ -3,11 +3,13 @@ var chaiHttp = require('chai-http');
 var expect = chai.expect;
 chai.use(chaiHttp);
 
+var config = require('../config.json');
+const HOSTNAME = config.hostname;
 
-describe('Book', function() {
+describe('Getting "book" objects:', function() {
   describe('#getByID()', function() {
     it('Should return a book with the given integer id.', function(done) {
-      chai.request("http://localhost:8080")
+      chai.request(HOSTNAME)
 	.get('/get/book/id/123')
         .end(function (err, res) {
           expect(err).to.be.null;
@@ -19,13 +21,37 @@ describe('Book', function() {
 
   describe('getByTitle()', function() {
     it('Should return a book with the given title.', function(done) {
-      chai.request("http://localhost:8080")
+      chai.request(HOSTNAME)
         .get("/get/book/title/MobyDick")
         .end(function (err, res) {
           expect(err).to.be.null;
           expect(res).to.have.status(200);
           done();
         });
-    })
+    });
+  });
+
+  describe('getByAuthor()', function() {
+    it('Should return all books by the given author.', function(done) {
+      chai.request(HOSTNAME)
+        .get("/get/book/author/JohnDoe")
+        .end(function (err, res) {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+  });
+
+  describe('getAll()', function() {
+    it('Should return all books.', function(done) {
+      chai.request(HOSTNAME)
+        .get("/get/book/all")
+        .end(function (err, res) {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
   });
 });
--- test/test-reading.js
+++ test/test-reading.js
@@ -3,11 +3,13 @@ var chaiHttp = require('chai-http');
 var expect = chai.expect;
 chai.use(chaiHttp);
 
+var config = require('../config.json');
+const HOSTNAME = config.hostname;
 
-describe('Reading', function() {
+describe('Getting "reading" objects:', function() {
   describe('#getByID()', function() {
     it('Should return a reading with the given integer id.', function(done) {
-      chai.request("http://localhost:8080")
+      chai.request(HOSTNAME)
 	.get('/get/reading/id/123')
         .end(function (err, res) {
           expect(err).to.be.null;
@@ -17,15 +19,39 @@ describe('Reading', function() {
     });
   });
 
-  describe('getByTitle()', function() {
+  describe('#getByTitle()', function() {
     it('Should return all readings for the book with the given title.', function(done) {
-      chai.request("http://localhost:8080")
+      chai.request(HOSTNAME)
         .get("/get/reading/title/MobyDick")
         .end(function (err, res) {
           expect(err).to.be.null;
           expect(res).to.have.status(200);
           done();
         });
-    })
+    });
+  });
+
+  describe('#getByAuthor()', function() {
+    it('Should return all readings of any book by the given author.', function(done) {
+      chai.request(HOSTNAME)
+        .get("/get/reading/author/JohnDoe")
+        .end(function (err, res) {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+  });
+
+  describe('#getAll()', function() {
+    it('Should return all readings.', function(done) {
+      chai.request(HOSTNAME)
+        .get("/get/reading/all")
+        .end(function (err, res) {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
   });
 });
